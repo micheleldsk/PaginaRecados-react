3e0e0d165efb75b728fec3091fb6d883e01553d4
micheleldsk <micheleldsk@gmail.com>
2022-11-23 17:36:55 -0300

    add findMsgById;

--- src/components/modal/MeuModal.tsx
+++ src/components/modal/MeuModal.tsx
@@ -1,13 +1,14 @@
 import { Modal, Typography, Paper } from '@mui/material'
 import { MeuButton } from '../button/MeuButton';
 import { MeuInput } from '../input/MeuInput';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addMsg, deleteMsg, msgSelectAll, resetModalMsg, resetSelectId } from '../../store/msgSlice';
+import { addMsg, deleteMsg, editMsg, msgSelectAll, resetModalMsg, resetSelectId } from '../../store/msgSlice';
 import { MeuAlert, MeuAlertProps } from '../alert/MeuAlert';
 import Message from '../global-types/TMessage';
 import { v4 } from 'uuid';
 import { useRevalidator } from 'react-router-dom';
+import { findMsgById } from './utils/findMsgById';
 
 type ModalProps = {
     open: boolean,
@@ -22,9 +23,19 @@ export const MeuModal = (props: ModalProps)  => {
 
     const [titulo, setTitulo] = useState('')
     const [mensagem, setMensagem] = useState('')
+    const [editTitulo, setEditTitulo] = useState('')
+    const [editMensagem, setEditMensagem] = useState('')
     const [alert, setAlert] = useState<MeuAlertProps>({texto: ''})
     
     const userId = JSON.parse(localStorage.getItem('usuarioLogado')!)
+
+    useEffect(() => {
+        if (msgRedux.selectId) {
+            const tempMsg = findMsgById(msgRedux.selectId, msgRedux.msgList)
+            setEditTitulo(tempMsg.titulo)
+            setEditMensagem(tempMsg.mensagem)
+        }
+    }, [msgRedux.selectId])
 
     function handleClose () {
         dispatch(resetModalMsg())   
@@ -53,6 +64,29 @@ export const MeuModal = (props: ModalProps)  => {
                 }, 2000);
                 setTimeout(() => {
                     dispatch(addMsg(newMsg))
+                    handleClose()
+                }, 2100);
+    }
+
+    function editMensagemById () {
+        if(!editTitulo || !editMensagem) {
+            setAlert ({texto:'Os campos devem ser preenchidos!', severity:'error'})
+                setTimeout(() => {
+                    setAlert({texto:''})
+                }, 2000);
+                return
+        }
+        const upDateMsg = {
+            id: msgRedux.selectId,
+            titulo: editTitulo,
+            mensagem: editMensagem
+        }
+        setAlert ({texto:'Mensagem alterada com SUCESSO!', severity:'success'})
+                setTimeout(() => {
+                    setAlert({texto:''})
+                }, 2000);
+                setTimeout(() => {
+                    dispatch(editMsg(upDateMsg))
                     handleClose()
                 }, 2100);
     }
@@ -92,11 +126,11 @@ export const MeuModal = (props: ModalProps)  => {
             <Typography sx={{ fontSize:'clamp(1.5rem, 2vw, 2rem)'}} align='center'>Editando Mensagem</Typography>
                     
             <MeuInput variant='outlined' label='Título' type='text' placeholder='Título'
-                      color='primary' size='medium' value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
+                      color='primary' size='medium' value={editTitulo} onClick={() => setEditTitulo('')} onChange={(e) => setEditTitulo(e.target.value)}/>
             <MeuInput variant='outlined' label='Mensagem' type='text' placeholder='Mensagem'
-                      color='primary' size='medium' value={mensagem} onChange={(e) => setMensagem(e.target.value)}/>
+                      color='primary' size='medium' value={editMensagem} onClick={() => setEditMensagem('')} onChange={(e) => setEditMensagem(e.target.value)}/>
                     
-            <MeuButton variant='contained' color='success' size='medium' texto='SALVAR'/>
+            <MeuButton variant='contained' color='success' size='medium' texto='SALVAR' onClick={editMensagemById}/>
             <MeuButton variant='contained' color='error' size='medium' texto='CANCELAR' onClick={handleClose}/>               
             </>
         )}
